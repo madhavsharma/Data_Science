087f06108993d69acc4669a2dd5fd6db9f557722
madhavsharma <mail.madhavsharma@gmail.com>
2013-08-23 14:45:04 +0530

    Generating inverted index using Mapreduce Framework

---
+++ MapReduce/inverted_index.py
@@ -0,0 +1,43 @@
+'''
+
+@Author : Madhav
+
+@Summary: Calculates the inverted index.
+
+'''
+
+
+import MapReduce
+import sys
+import json
+
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    doc_id = record[0]
+    content = record[1]
+    #print doc_id,content
+    word_list = []
+    words = content.split()
+    for w in words:
+		if w not in word_list:
+			word_list.append(w)
+			mr.emit_intermediate(w,doc_id)
+		else:
+			continue
+
+def reducer(key, list_of_values):
+    inverted_index = []
+    for v in list_of_values:
+      inverted_index.append(v)
+    mr.emit((key,inverted_index))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
