ff0ef114ff8090b2dff65c130e1326e86f354366
madhavsharma <mail.madhavsharma@gmail.com>
2013-08-23 14:11:24 +0530

    counting the relationship 'Friend' from key-value pair available in social network dataset

---
+++ MapReduce/friend_count.py
@@ -0,0 +1,31 @@
+import MapReduce
+import sys
+import json
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+	key = record[0]
+	value = record[1]
+	mr.emit_intermediate(key,1)
+
+def reducer(key, list_of_values):
+	total = 0
+	for value in list_of_values:
+		total+= value
+	mr.emit((key,total))
+	
+
+
+		
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
